049289f5714467bc9a529690afddb4770dfacdf1
emirfb7 <129848831+emirfb7@users.noreply.github.com>
2026-05-26 22:36:58 +0300

    fix assets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,13 @@
 <title>Zinar Zirai İlaç Satışı</title>
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=JetBrains+Mono:wght@400;600&display=swap" rel="stylesheet"/>
-<link rel="stylesheet" href="/css/style.css"/>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
 <script src="https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 @include('partials.body')
-<script src="/js/db.js"></script>
-<script src="/js/app.js"></script>
+<script src="{{ asset('js/db.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
